2e71837e0a7eb6d4ccfb8c1f0eb382e35897001e
Claire Pritchard <claire.m.pritchard@gmail.com>
2017-04-19 13:14:30 +0200

    Added search to Place Admin map with ol3-geocoder

--- templates/admin/place_admin_map.js
+++ templates/admin/place_admin_map.js
@@ -1,6 +1,5 @@
 {% load staticfiles %}
 {% load l10n %}
-
 {% block vars %}
 INITIAL_ZOOM = 5;
 FEATURE_ZOOM = 9;
@@ -12,19 +11,19 @@ var wkt_format;
 {% endblock %}
 
 // Read WKT from the text field
-function read_wkt (wkt) {
+function read_wkt(wkt) {
     var wkt = document.getElementById('{{ id }}').value;
     var feature;
-    if  (wkt) {
+    if (wkt) {
         feature = wkt_format.readFeature(wkt);
     }
     return feature;
 };
 
 // Write WKT of given feature to text field
-function write_wkt(feature){
-     var wkt = 'SRID={{ srid|unlocalize }};' + wkt_format.writeFeature(feature);
-     document.getElementById('{{ id }}').value = wkt;
+function write_wkt(feature) {
+    var wkt = 'SRID={{ srid|unlocalize }};' + wkt_format.writeFeature(feature);
+    document.getElementById('{{ id }}').value = wkt;
 };
 
 function map_init() {
@@ -42,7 +41,7 @@ function map_init() {
     }
 
     var iconStyle = new ol.style.Style({
-        image: new ol.style.Icon( ({
+        image: new ol.style.Icon(({
             anchor: [0.5, 41],
             anchorXUnits: 'fraction',
             anchorYUnits: 'pixels',
@@ -60,12 +59,12 @@ function map_init() {
         type: 'Point'
     });
 
-    draw.on('drawstart', function(evt) {
+    draw.on('drawstart', function (evt) {
         // Only one feature at a time allowed on map
         vectorSource.clear();
     });
 
-    draw.on('drawend', function(evt) {
+    draw.on('drawend', function (evt) {
         // Get coordinates of point just added
         write_wkt(evt.feature);
     });
@@ -95,5 +94,28 @@ function map_init() {
         // Make sure map isn't zoomed in too far
         view.setZoom(FEATURE_ZOOM);
     }
+
+    // Use ol3-geocoder to search for place names in the map
+    var geocoder = new Geocoder('nominatim', {
+        provider: 'osm',
+        autoComplete: true,
+        lang: 'en-US',
+        placeholder: 'Search for ...',
+        targetType: 'glass-button',
+        limit: 8,
+        keepOpen: false,
+        preventDefault : true // don't automatically place feature on map
+    });
+    map.addControl(geocoder);
+
+    geocoder.on('addresschosen', function (evt) {
+        var feature = evt.feature,
+            coord = evt.coordinate,
+            address = evt.address;
+        // Center and zoom in to chosen address
+        var view = map.getView();
+        view.setCenter(coord);
+        view.setZoom(FEATURE_ZOOM);
+    });
     {% endblock %}
 };
