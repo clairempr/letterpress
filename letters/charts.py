8e5d3ac33ff21f81d30adc5278b7b909c015f069
Me <clairempr@users.noreply.github.com>
2017-10-07 16:07:42 +0200

    Use pandas dataframes when creating bokeh charts

--- letters/charts.py
+++ letters/charts.py
@@ -17,22 +17,30 @@ PALETTE = ['#4582ec', '#a6a6a6', '#aed5f9', '#5bc0de', '#6699ef']
 def make_charts(words, months, proportions, word_freqs, totals, averages, doc_counts):
     charts = []
 
-    # Freqency chart
-    fcharts = get_frequency_charts(words, months, word_freqs)
+    # Make pandas dataframe with word frequencies
+    # word_freqs is a list of word frequencies, grouped by month and then by word
+    frequency_df = pd.DataFrame()
+    for idx, word in enumerate(words):
+        frequency_df[word] = word_freqs[idx::len(words)]
+    frequency_df['Month'] = months
+
+    fcharts = get_frequency_charts(words, frequency_df)
 
     # Make proportions chart if 2 words were searched for
     if len(words) == 2:
-        proportions_chart = get_proportions_chart(words, months, proportions)
+        proportions_df = pd.DataFrame({'Month': months, 'Proportion': proportions})
+        proportions_chart = get_proportions_chart(words, proportions_df)
         fcharts.append(proportions_chart)
 
     # Totals chart
-    totals_chart = get_per_month_chart(months, totals, 'Total words per month', 'Total words')
-
+    totals_chart = get_per_month_chart(pd.DataFrame({'Month': months, 'Total': totals}),
+                                               'Total words per month', 'Total words')
     # Averages chart
-    averages_chart = get_per_month_chart(months, averages, 'Average words per letter', 'Average words')
-
+    averages_chart = get_per_month_chart(pd.DataFrame({'Month': months, 'Average': averages}),
+                                                 'Average words per letter', 'Average words')
     # Number of letters chart
-    doc_count_chart = get_per_month_chart(months, doc_counts, 'Letters per month', 'Letters')
+    doc_count_chart = get_per_month_chart(pd.DataFrame({'Month': months, 'Average': doc_counts}),
+                                                  'Letters per month', 'Letters')
 
     charts.append(row(children=fcharts, responsive=True))
     charts.append(row(totals_chart, doc_count_chart, averages_chart, responsive=True))
@@ -44,55 +52,39 @@ def make_charts(words, months, proportions, word_freqs, totals, averages, doc_co
                               {'script': script, 'divs': divs})
 
 
-def get_frequency_charts(words, months, word_freqs):
+def get_frequency_charts(words, df):
     title = 'Frequency of '
     for idx, word in enumerate(words):
         if idx > 0:
             title += ' and '
         title += str.format('"{0}"', word)
 
-    # best support for bar chart is with data in a format that is table-like
-    bar_words = []
-    bar_months = []
-    for month in months:
-        bar_words.extend(words)
-        for i in range(len(words)):
-            bar_months.append(month)
+    time_series = TimeSeries(df, title=title, y=words,
+                             x='Month',
+                             xlabel='Month', ylabel='Frequency',
+                             legend='top_right',
+                             palette=PALETTE, toolbar_location='right')
 
-    data = {
-        'word': bar_words,
-        'month': bar_months,
-        'frequency': word_freqs
-    }
-
-    # x-axis labels pulled from the months column, stacking labels from words column
-    bar = Bar(data, values='frequency', label='month', group='word',#stack='word',
+    df = pd.melt(df, id_vars='Month').dropna().set_index('Month')
+    bar = Bar(df, values='value', group='variable', label='Month',
+              ylabel='Frequency',
               title=title, legend='top_right', bar_width=1,
               palette=PALETTE, toolbar_location='right')
 
-    data = {
-        'month': months,
-    }
-    for idx, word in enumerate(words):
-        data[word] = word_freqs[idx::len(words)]
-
-    time_series = TimeSeries(data, title=title, x='month', y=words, xlabel='Month', ylabel='Frequency',
-                             legend='top_right', palette=PALETTE, toolbar_location='right')
     return [bar, time_series]
 
 
-def get_proportions_chart(words, months, proportions):
+def get_proportions_chart(words, df):
     title = str.format('Proportions of "{0}" to "{1}"', words[0], words[1])
-    s = pd.Series(proportions, index=months)
-    time_series = TimeSeries(s, title=title, xlabel='Month', ylabel='Proportion', legend=False,
+    time_series = TimeSeries(df, title=title, x='Month',
+                             xlabel='Month', ylabel='Proportion', legend=False,
                              palette=PALETTE, toolbar_location='right')
     return time_series
 
 
-def get_per_month_chart(months, values, title, label):
-    s = pd.Series(values, index=months)
-    time_series = TimeSeries(s, title=title, xlabel='Month', ylabel=label, legend=False,
+def get_per_month_chart(df, title, label):
+    time_series = TimeSeries(df, title=title, x='Month',
+                             xlabel='Month', ylabel=label, legend=False,
                              palette=PALETTE, toolbar_location='right')
     return time_series
-
 
