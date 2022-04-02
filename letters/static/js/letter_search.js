be80f6196d0d4f0407a76c002cad8d8add7c8179
Claire <clairempr@users.noreply.github.com>
2022-04-01 23:40:49 -0400

    Added QUnit tests for letter_search.js

--- letters/static/js/letter_search.js
+++ letters/static/js/letter_search.js
@@ -1,103 +1,122 @@
 jQuery(document).ready(function ($) {
-    var last_page = 0;
+    let last_page = 0;
 
     // If coming back to this page after a letter view, show the search results again
     if (history.state) {
-        restore_search_results(history.state.result, history.state.pagination);
+        search_results.restore(history.state.result, history.state.pagination);
     }
 
     // Enable going back and forth between pages of search results with back button
     window.addEventListener('popstate', function (event) {
         if (event.state) {
-            restore_search_results(event.state.result, event.state.pagination);
+            search_results.restore(event.state.result, event.state.pagination);
         }
     }, false);
 
     $("#search_button").click(function () {
-        do_search(0);
+        alert('clicked');
+        search.do_search(0);
     });
 });
 
-function do_search(page_number) {
-    clear_active_page();
-    var filter_values = get_filter_values();
+let search = {
 
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        data: {
-            search_text: filter_values.search_text,
-            sources: filter_values.sources,
-            writers: filter_values.writers,
-            start_date: filter_values.start_date,
-            end_date: filter_values.end_date,
-            sentiments: filter_values.sentiments,
-            sort_by: filter_values.sort_by,
-            page_number: page_number,
-        },
-        url: "/search/",
-        success: function (result) {
-            show_search_results(result, page_number);
-            //window.location.hash = "#search_button";
+    do_search(page_number) {
+        active_page.clear();
+        var filter_values = get_filter_values();
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                search_text: filter_values.search_text,
+                sources: filter_values.sources,
+                writers: filter_values.writers,
+                start_date: filter_values.start_date,
+                end_date: filter_values.end_date,
+                sentiments: filter_values.sentiments,
+                sort_by: filter_values.sort_by,
+                page_number: page_number,
+            },
+            url: "/search/",
+            success: function (result) {
+                search_results.show(result, page_number);
+                //window.location.hash = "#search_button";
+            }
+        });
+    }
+
+}
+
+let search_results = {
+
+    show(result, page_number) {
+        $('#letters').html(result.letters);
+        if (result.pagination) {
+            pagination.set(result.pagination);
+            last_page = result.pages;
+            active_page.set(1);
+        } else {
+            active_page.set(page_number);
         }
-    });
-}
 
-function show_search_results(result, page_number) {
-    $('#letters').html(result.letters);
-    if (result.pagination) {
-        set_pagination(result.pagination);
+        var stateObj = {
+            result: result,
+            pagination: $('#pagination-top').get(0).innerHTML
+        };
+        history.replaceState(stateObj, '', '?page=' + page_number);
+    },
+
+    restore(result, pagination_to_restore) {
+        $('#letters').html(result.letters);
+        pagination.set(pagination_to_restore);
         last_page = result.pages;
-        set_active_page(1);
-    }
-    else {
-        set_active_page(page_number);
     }
 
-    var stateObj = {
-        result: result,
-        pagination: $('#pagination-top').get(0).innerHTML
-    };
-    history.replaceState(stateObj, '', '?page=' + page_number);
 }
 
-function restore_search_results(result, pagination) {
-    $('#letters').html(result.letters);
-    set_pagination(pagination);
-    last_page = result.pages;
-}
+let pagination = {
 
-function set_pagination(pagination) {
-    $('#pagination-top').html(pagination);
-    $('#pagination-bottom').html(pagination);
-}
-
-function set_active_page(new_page) {
-    $('li[name=page' + new_page + ']').addClass('active');
-}
-
-function clear_active_page() {
-    $('ul#pages.pagination').find('.active').removeClass('active');
-}
-
-function get_active_page() {
-    // Pagination at top and bottom of page. Just get the first.
-    var active_page_item = $('ul#pages.pagination').find('.active').get(0);
-    var active_page = parseInt(active_page_item.innerText);
-    return active_page;
-}
-
-function search_next_page() {
-    var active_page = get_active_page();
-    if (active_page < last_page) {
-        do_search(active_page + 1);
+    set(pagination_to_set) {
+        $('#pagination-top').html(pagination_to_set);
+        $('#pagination-bottom').html(pagination_to_set);
     }
+
 }
 
-function search_prev_page() {
-    var active_page = get_active_page();
-    if (active_page > 1) {
-        do_search(active_page - 1);
+let active_page = {
+
+    clear() {
+        $('ul#pages.pagination').find('.active').removeClass('active');
+    },
+
+    get() {
+        // Pagination at top and bottom of page. Just get the first.
+        var active_page_item = $('ul#pages.pagination').find('.active').get(0);
+        var the_active_page = parseInt(active_page_item.innerText);
+        return the_active_page;
+    },
+
+    set(new_page) {
+        $('li[name=page' + new_page + ']').addClass('active');
     }
+
 }
 
+let search_page = {
+
+    next() {
+        var the_active_page = active_page.get();
+        if (the_active_page < last_page) {
+            search.do_search(the_active_page + 1);
+        }
+    },
+
+    prev() {
+        var the_active_page = active_page.get();
+        if (the_active_page > 1) {
+            search.do_search(the_active_page - 1);
+        }
+    }
+
+}
---
+++ test/letter_search.test.js
@@ -0,0 +1,246 @@
+/* global QUnit, letter_search */
+'use strict';
+
+QUnit.module("letter_search", function (hooks) {
+  hooks.beforeEach(function (assert) {
+    // Search results will get put in "letters" div
+    $('<div id="pagination-top"></div>').appendTo('#qunit-fixture');
+    $('<div id="letters"></div>').appendTo('#qunit-fixture');
+    $('<div id="pagination-bottom"></div>').appendTo('#qunit-fixture');
+
+    this.active_page = 3;
+  });
+
+  //QUnit.module('search_button');
+
+  //QUnit.test("click", function (assert) {
+  //  let search_button = $("#search_button");
+  //  search_button.appendTo('#qunit-fixture');
+
+  //  let fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+
+  //  search_button.click(fake_do_search);
+
+  //  assert.ok(search.do_search.called, "search.do_search() has been called");
+
+  //  sinon.restore();
+  //});
+
+  QUnit.module('pagination');
+
+  QUnit.test('set', function (assert) {
+    const pagination_html = "This is the pagination html";
+    pagination.set(pagination_html);
+
+    assert.equal($('#pagination-top').html(), pagination_html, "Element with id pagination-top has pagination html set");
+    assert.equal($('#pagination-bottom').html(), pagination_html, "Element with id pagination-bottom has pagination html set");
+  });
+
+  QUnit.module('active page');
+
+  QUnit.test('clear', function (assert) {
+    $('<ul id="pages" class="pagination"><li class="active">'+this.active_page+'</li></ul>').appendTo('#qunit-fixture');
+
+    active_page.clear();
+
+    assert.notOk($('ul#pages.pagination li').hasClass("active"), "Page list item has had 'active' class removed");
+  });
+
+  QUnit.test('get', function (assert) {
+    // Pagination is at the top and bottom of the page
+    // get_active_page() is supposed to get the first
+
+    // Append top pagination to qunit-fixture
+    $('<ul id="pages" class="pagination"><li>1</li><li class="active">2</li></ul>').appendTo('#qunit-fixture');
+    // Append bottom pagination with different active page to qunit-fixture
+    $('<ul id="pages" class="pagination"><li class="active">1</li></ul>').appendTo('#qunit-fixture');
+
+    let the_active_page = active_page.get();
+
+    assert.equal(the_active_page, 2, "Active page is first one with 'active' class");
+  });
+
+  QUnit.test('set', function (assert) {
+    const new_page = 3;
+    active_page.set(new_page);
+    assert.notOk($("li[name=page" + new_page + "]").hasClass("active"), "Current page list item has had 'active' class set");
+  });
+
+  QUnit.module('search');
+
+  QUnit.test('do_search', function (assert) {
+    // search.do_search() should call active_page.clear() and make an ajax call
+    // If ajax call not successful, search_results.show() shouldn't be called
+    let fake_clear_active_page = sinon.replace(active_page, "clear", sinon.fake());
+    let fake_ajax = sinon.replace(jQuery, "ajax", sinon.fake());
+    let fake_show_search_results = sinon.replace(search_results, "show", sinon.fake());
+
+    search.do_search(
+      fake_clear_active_page,
+      fake_ajax,
+      fake_show_search_results
+    );
+
+    assert.ok(active_page.clear.called, "active_page.clear() has been called");
+    assert.ok(jQuery.ajax.called, "An ajax call has been made");
+    assert.notOk(search_results.show.called, "search_results.show() has not been called");
+
+    sinon.restore();
+
+    // If ajax call was successful, search_results.show() should be called
+    const letters = "These are the letters";
+    const pages = "These are the pages";
+    const data = {
+      letters: letters,
+      pages: pages
+    }
+    sinon.stub(jQuery, "ajax").yieldsTo("success", data);
+    fake_show_search_results = sinon.replace(search_results, "show", sinon.fake());
+
+    search.do_search(
+      fake_show_search_results
+    );
+
+    assert.ok(search_results.show.called, "search_results.show() has been called");
+
+    sinon.restore();
+  });
+
+  QUnit.module('search page', {
+    beforeEach: function () {
+      $('<ul id="pages" class="pagination"><li class="active">'+this.active_page+'</li></ul>').appendTo('#qunit-fixture');
+    },
+  });
+
+    QUnit.test('next', function (assert) {
+      let fake_get_active_page = sinon.replace(active_page, "get", sinon.fake.returns(2));
+      let fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+      // if the_active_page.get() >= last_page, search.do_search() shouldn't get called
+      last_page = 1;
+
+      search_page.next(fake_get_active_page, fake_do_search);
+
+      assert.ok(active_page.get.called, "active_page.get() has been called");
+      assert.notOk(search.do_search.called, "search.do_search() hasn't been called");
+
+      sinon.restore();
+
+      // if the_active_page.get() >= last_page, search.do_search() shouldn't get called
+      last_page = 2;
+
+      sinon.replace(active_page, "get", sinon.fake.returns(2));
+      fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+
+      search_page.next(fake_do_search);
+
+      assert.notOk(search.do_search.called, "search.do_search() hasn't been called");
+
+      sinon.restore();
+
+      // if the_active_page.get() < last_page, search.do_search() should get called
+      last_page = 3;
+
+      sinon.replace(active_page, "get", sinon.fake.returns(2));
+      fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+
+      search_page.next(fake_do_search);
+
+      assert.ok(search.do_search.called, "search.do_search() has been called");
+
+      sinon.restore();
+    });
+
+    QUnit.test('prev', function (assert) {
+      let fake_get_active_page = sinon.replace(active_page, "get", sinon.fake.returns(1));
+      let fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+      last_page = 0;
+
+      search_page.prev(fake_get_active_page, fake_do_search);
+
+      assert.ok(active_page.get.called, "active_page.get() has been called");
+      // if the_active_page.get() <= 1, search.do_search() shouldn't get called
+      assert.notOk(search.do_search.called, "search.do_search() has been called");
+
+      sinon.restore();
+
+      // if the_active_page.get() <= 1, search.do_search() shouldn't get called
+      last_page = 1;
+
+      sinon.replace(active_page, "get", sinon.fake.returns(1));
+      fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+
+      search_page.prev(fake_get_active_page, fake_do_search);
+
+      assert.notOk(search.do_search.called, "search.do_search() has been called");
+
+      sinon.restore();
+
+      // if active_page.get() > 1, search.do_search() should get called
+      last_page = 1;
+
+      sinon.replace(active_page, "get", sinon.fake.returns(2));
+      fake_do_search = sinon.replace(search, "do_search", sinon.fake());
+
+      search_page.prev(fake_get_active_page, fake_do_search);
+
+      assert.ok(search.do_search.called, "search.do_search() has been called");
+
+      sinon.restore();
+    });
+
+  QUnit.module('search results', {
+    before: function () {
+      this.letters = "These are the letters";
+      this.pages = 5;
+      this.result = {
+        letters: this.letters,
+        pages: this.pages
+      };
+    },
+  });
+
+  QUnit.test('restore', function (assert) {
+    this.pagination = "This is the pagination";
+
+    let fake_set_pagination = sinon.replace(pagination, "set", sinon.fake());
+
+    search_results.restore(this.result, this.pagination, fake_set_pagination);
+
+    assert.equal($('#letters').html(), this.letters, "Element with id letters has letters html set");
+    assert.ok(pagination.set.called, "pagination.set() has been called");
+
+    sinon.restore();
+  });
+
+  QUnit.test('show', function (assert) {
+    let fake_set_pagination = sinon.replace(pagination, "set", sinon.fake());
+    let fake_set_active_page = sinon.replace(active_page, "set", sinon.fake());
+
+    sinon.replace(history, "replaceState", sinon.fake());
+
+    search_results.show(this.result, 3, fake_set_pagination, fake_set_active_page);
+
+    assert.equal($('#letters').html(), this.letters, "Element with id letters has letters html set");
+    assert.ok(history.replaceState.called, "history.replaceState() has been called");
+    assert.ok(active_page.set.called, "active_page.set() has been called");
+
+    // If result.pagination isn't filled, pagination.set() shouldn't get called
+    assert.notOk(pagination.set.called, "pagination.set() hasn't been called");
+
+    sinon.restore();
+
+    // If result.pagination is filled, pagination.set() should get called
+    this.result.pagination = "This is the pagination";
+
+    fake_set_pagination = sinon.replace(pagination, "set", sinon.fake());
+    fake_set_active_page = sinon.replace(active_page, "set", sinon.fake());
+
+    search_results.show(this.result, 3, fake_set_pagination, fake_set_active_page);
+
+    assert.ok(pagination.set.called, "pagination.set() has been called");
+    assert.ok(active_page.set.called, "active_page.set() has been called");
+
+    sinon.restore();
+  });
+
+});
